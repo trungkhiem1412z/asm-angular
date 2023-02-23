ecd442c26deba70383e2ed9615b93fa505fa9d1e
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-02-23 11:16:30 +0700

    update

--- src/app/guard/admin-auth.guard.ts
+++ src/app/guard/admin-auth.guard.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminAuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -16,6 +17,8 @@ export class AdminAuthGuard implements CanActivate {
     if (currentUser && currentUser.role === 'admin') {
       return true;
     }
+    // Người dùng không có quyền admin
+    this.router.navigate(['/home']);
     return false;
   }
 }
